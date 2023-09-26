bd5fb4cab01293fa119b8af8dc96e7da5753ba45
HAMMYND <144380945+HAMMYND@users.noreply.github.com>
2023-09-26 13:28:50 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 document.getElementById('heading').innerHTML="hello world"
+let normal_rate = 10 
+let overtime_rate = 20
+let hours_worked = 10 
+let normal_hours = 8 
+
+if (hours_worked <= normal_hours) {
+    money = hours_worked * normal_rate
+} else {
+    money = normal_rate * normal_hours + (hours_worked-normal_hours)*overtime_rate
+}
